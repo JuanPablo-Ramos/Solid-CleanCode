77806bac96870449a179f69fc23b38c17c6e2629
JuanPablo-Ramos <juanp.ramos@udea.edu.co>
2024-04-14 20:49:38 -0500

    First Commit
    Creation of Main class and secondary classes (Product, ModelProductsTable).
    
    Co-Authored-By: FSPOISON <128433342+fspoison@users.noreply.github.com>

---
+++ src/main/java/com/mycompany/solid/cleancode/Product.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.solid.cleancode;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Product {
+    
+    private Integer productId;
+    private float price; 
+    private String name;
+    private Integer stock;
+
+    public Product(Integer productId, String name, Integer stock, float price) {
+        this.productId = productId;
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    /**
+     * @return the productId
+     */
+    public Integer getProductId() {
+        return productId;
+    }
+
+    /**
+     * @param productId the productId to set
+     */
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the stock
+     */
+    public Integer getStock() {
+        return stock;
+    }
+
+    /**
+     * @param stock the stock to set
+     */
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    
+}
