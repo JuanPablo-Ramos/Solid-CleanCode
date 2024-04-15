42e407292e78464fe01ca11ec9f0e462a27e2651
JuanPablo-Ramos <juanp.ramos@udea.edu.co>
2024-04-14 19:45:02 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/solid/cleancode/SolidCleanCode.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solid.cleancode;
+
+/**
+ *
+ * @author Usuario
+ */
+public class SolidCleanCode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
